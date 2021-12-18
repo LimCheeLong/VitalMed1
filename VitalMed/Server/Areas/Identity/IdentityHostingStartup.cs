9b69a2e7f489416697abe1e0568b87a471575117
LIM CHEE LONG 2002318J <2002318J@student.tp.edu.sg>
2021-12-18 17:35:22 +0800

    Project

---
+++ VitalMed/Server/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using VitalMed.Server.Data;
+using VitalMed.Server.Models;
+
+[assembly: HostingStartup(typeof(VitalMed.Server.Areas.Identity.IdentityHostingStartup))]
+namespace VitalMed.Server.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
