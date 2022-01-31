1eda7cc12139647fe1e8ffd6210db742fef22bdf
LIM CHEE LONG 2002318J <2002318J@student.tp.edu.sg>
2022-02-01 05:12:24 +0800

    .

--- VitalMed/Shared/Domain/Customer.cs
+++ VitalMed/Shared/Domain/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,11 @@ namespace VitalMed.Shared.Domain
     public class Customer
     {
         public int ID { get; set; }
+        [Required]
         public string Address { get; set; }
+        [Required]
         public int ContactNumber { get; set; }
+        [Required]
         public string Email { get; set; }
         public virtual CartItem Cart { get; set; }
     }
--- VitalMed/Shared/Domain/Product.cs
+++ VitalMed/Shared/Domain/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,11 @@ namespace VitalMed.Shared.Domain
     public class Product
     {
         public int ID { get; set; }
+        [Required]
         public string ProductName { get; set; }
+        [Required]
         public double ProductPrice { get; set;}
+        [Required]
         public string ProductDesc { get; set; }
         public string ProductImage { get; set; }
         public virtual Category Category { get; set; }
